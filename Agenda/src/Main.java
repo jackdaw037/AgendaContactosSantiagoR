759c7a4f851bfdbcfa79c915b024ebd427b11dd8
santiago <santisalserin.sr@gmail.com>
2025-09-26 10:03:03 +0200

    Update Main.java
    implementacion menu

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args){
 
-        System.out.println("hello");
+    Scanner sc = new Scanner(System.in);
+
+
+
+    System.out.println("añadir contacto ");
+    System.out.println("mostrar contactos ");
+    System.out.println("Buscar contactos ");
+    System.out.println("salir ");
+
+    System.out.println("seleccione la opcion");
+
+    String menu = sc.next();
+    String menu1 = menu;
+
+    System.out.println("has elegido la opccion : "+menu1);
+
 
     }
 }
