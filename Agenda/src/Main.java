debe282bcc1c15df1142d899f86eacf8efb29b7c
santiago <santisalserin.sr@gmail.com>
2025-10-29 12:36:52 +0100

    Update Main.java

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,24 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("1. Añadir contacto");
+        System.out.println("2. Mostrar contactos");
+        System.out.println("3. Buscar contactos");
+        System.out.println("4. Salir");
+        System.out.print("Seleccione una opción: ");
 
-
-    System.out.println("añadir contacto ");
-    System.out.println("mostrar contactos ");
-    System.out.println("Buscar contactos ");
-    System.out.println("salir ");
-
-    System.out.println("seleccione la opcion");
-
-    String menu = sc.next();
-    String menu1 = menu;
-
-    System.out.println("has elegido la opccion : "+menu1);
+        int menuOpcion = sc.nextInt();
 
 
+
+        switch (menuOpcion) {
+            case 1:
+                System.out.println("Usted ha elegido la opción 1");
+                System.out.println("Añadir contacto");
+                break;
+
+            case 2:
+                System.out.println("Usted ha elegido la opción 2");
+                System.out.println("Mostrar contactos");
+                break;
+
+            case 3:
+                System.out.println("Usted ha elegido la opción 3");
+                System.out.println("Buscar contactos");
+                break;
+
+            case 4:
+                System.out.println("Usted ha elegido la opción 4");
+                System.out.println("Salir del programa...");
+                break;
+
+            default:
+                System.out.println("Opción inválida");
+                break;
+        }
+
+        sc.close();
     }
 }
