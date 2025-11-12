89dedbaf42353624c8377f0385144d5e430f57bf
jackdaw037 <santisalserin.sr@gmail.com>
2025-11-12 12:35:05 +0100

    2

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,46 +1,52 @@
 import java.util.Scanner;
 
-public class Main {
+public class Primero {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int menuOpcion = 0;
+        boolean con = true;
 
-        System.out.println("1. Añadir contacto");
-        System.out.println("2. Mostrar contactos");
-        System.out.println("3. Buscar contactos");
-        System.out.println("4. Salir");
-        System.out.print("Seleccione una opción: ");
+        while (con == true) {
 
-        int menuOpcion = sc.nextInt();
+            System.out.println("1. Añadir contacto");
+            System.out.println("2. Mostrar contactos");
+            System.out.println("3. Buscar contactos");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            menuOpcion = sc.nextInt();
 
 
+            switch (menuOpcion) {
+                case 1:
+                    System.out.println("Usted ha elegido la opción 1");
+                    System.out.println("Añadir contacto");
+                    break;
 
-        switch (menuOpcion) {
-            case 1:
-                System.out.println("Usted ha elegido la opción 1");
-                System.out.println("Añadir contacto");
-                break;
+                case 2:
+                    System.out.println("Usted ha elegido la opción 2");
+                    System.out.println("Mostrar contactos");
+                    break;
 
-            case 2:
-                System.out.println("Usted ha elegido la opción 2");
-                System.out.println("Mostrar contactos");
-                break;
+                case 3:
+                    System.out.println("Usted ha elegido la opción 3");
+                    System.out.println("Buscar contactos");
+                    break;
 
-            case 3:
-                System.out.println("Usted ha elegido la opción 3");
-                System.out.println("Buscar contactos");
-                break;
+                case 4:
+                    System.out.println("Usted ha elegido la opción 4");
+                    System.out.println("Salir del programa...");
+                    con = false;
+                    break;
 
-            case 4:
-                System.out.println("Usted ha elegido la opción 4");
-                System.out.println("Salir del programa...");
-                break;
+                default:
+                    System.out.println("Opción inválida");
+                    break;
+            }
 
-            default:
-                System.out.println("Opción inválida");
-                break;
         }
-
         sc.close();
+
     }
 }
