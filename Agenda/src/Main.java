cf39db986e8cd17ee7ab8c64888fce85d8671fb8
santiago <santisalserin.sr@gmail.com>
2025-12-10 11:58:46 +0100

    Update Main.java

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,52 +1,101 @@
 import java.util.Scanner;
 
-public class Primero {
+public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int menuOpcion = 0;
         boolean con = true;
+        
+        String[] list = new String[100];
 
-        while (con == true) {
+        while (con) {
 
             System.out.println("1. Añadir contacto");
             System.out.println("2. Mostrar contactos");
             System.out.println("3. Buscar contactos");
-            System.out.println("4. Salir");
-            System.out.print("Seleccione una opción: ");
+            System.out.println("4. Editar contacto");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opcion: ");
 
             menuOpcion = sc.nextInt();
-
+            sc.nextLine();
 
             switch (menuOpcion) {
+                
                 case 1:
-                    System.out.println("Usted ha elegido la opción 1");
+                    System.out.println("Usted ha elegido la opcion 1");
                     System.out.println("Añadir contacto");
-                    break;
 
+                    System.out.print("Ingrese nombre: ");
+                    String nombre = sc.nextLine();
+
+                    System.out.print("Ingrese numero de telefono: ");
+                    String telefono = sc.nextLine(); 
+
+                    String contacto = "Nombre: " + nombre + " | Telefono: " + telefono;
+
+                    boolean agregado = false;
+
+                    for (int i = 0; i < list.length; i++) {
+                        if (list[i] == null) {
+                            list[i] = contacto;   
+                            agregado = true;
+                            break;
+                        }
+                    }
+
+                    if (agregado) {
+                        System.out.println("Contacto agregado correctamente.");
+                    } else {
+                        System.out.println("La agenda esta llena.");
+                    }
+
+                    break;
+                    
                 case 2:
-                    System.out.println("Usted ha elegido la opción 2");
+                    System.out.println("Usted ha elegido la opcion 2");
                     System.out.println("Mostrar contactos");
-                    break;
 
+                    boolean vacio = true;
+
+                    for (String c : list) {
+                        if (c != null) {
+                            System.out.println(c);
+                            vacio = false;
+                        }
+                    }
+
+                    if (vacio) {
+                        System.out.println("No hay contactos guardados.");
+                    }
+
+                    break;
+                    
                 case 3:
                     System.out.println("Usted ha elegido la opción 3");
                     System.out.println("Buscar contactos");
                     break;
 
+                    
                 case 4:
                     System.out.println("Usted ha elegido la opción 4");
-                    System.out.println("Salir del programa...");
+                    System.out.println("Editar contacto");
+                    break;
+
+                    
+                case 0:
+                    System.out.println("Saliendo del programa...");
                     con = false;
                     break;
+
 
                 default:
                     System.out.println("Opción inválida");
                     break;
             }
-
         }
-        sc.close();
 
+        sc.close();
     }
 }
