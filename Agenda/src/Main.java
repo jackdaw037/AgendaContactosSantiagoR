7da944c66fb5932259eba653a1151576bb3652bf
santiago <santisalserin.sr@gmail.com>
2026-02-09 17:30:39 +0100

    Update Main.java

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    static ArrayList<String> nombres = new ArrayList<>();
+    static ArrayList<String> telefonos = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int menuOpcion = 0;
+        int menuOpcion;
         boolean con = true;
-        
-        String[] list = new String[100];
 
         while (con) {
 
@@ -22,80 +25,57 @@ public class Main {
             sc.nextLine();
 
             switch (menuOpcion) {
-                
+
                 case 1:
-                    System.out.println("Usted ha elegido la opcion 1");
-                    System.out.println("Añadir contacto");
-
-                    System.out.print("Ingrese nombre: ");
-                    String nombre = sc.nextLine();
-
-                    System.out.print("Ingrese numero de telefono: ");
-                    String telefono = sc.nextLine(); 
-
-                    String contacto = "Nombre: " + nombre + " | Telefono: " + telefono;
-
-                    boolean agregado = false;
-
-                    for (int i = 0; i < list.length; i++) {
-                        if (list[i] == null) {
-                            list[i] = contacto;   
-                            agregado = true;
-                            break;
-                        }
-                    }
-
-                    if (agregado) {
-                        System.out.println("Contacto agregado correctamente.");
-                    } else {
-                        System.out.println("La agenda esta llena.");
-                    }
-
+                    agregarContacto();
                     break;
-                    
+
                 case 2:
-                    System.out.println("Usted ha elegido la opcion 2");
-                    System.out.println("Mostrar contactos");
-
-                    boolean vacio = true;
-
-                    for (String c : list) {
-                        if (c != null) {
-                            System.out.println(c);
-                            vacio = false;
-                        }
-                    }
-
-                    if (vacio) {
-                        System.out.println("No hay contactos guardados.");
-                    }
-
+                    mostrarContacto();
                     break;
-                    
+
                 case 3:
-                    System.out.println("Usted ha elegido la opción 3");
                     System.out.println("Buscar contactos");
                     break;
 
-                    
                 case 4:
-                    System.out.println("Usted ha elegido la opción 4");
                     System.out.println("Editar contacto");
                     break;
 
-                    
                 case 0:
                     System.out.println("Saliendo del programa...");
                     con = false;
                     break;
 
-
                 default:
                     System.out.println("Opción inválida");
-                    break;
             }
         }
+    }
 
-        sc.close();
+    static void agregarContacto() {
+
+        System.out.print("Inserte nombre: ");
+        String nuevoNombre = sc.nextLine();
+
+        System.out.print("Inserte telefono: ");
+        String nuevoTelefono = sc.nextLine();
+
+        nombres.add(nuevoNombre);
+        telefonos.add(nuevoTelefono);
+
+        System.out.println("El contacto se ha agregado correctamente");
+    }
+
+    static void mostrarContacto() {
+        if (nombres.isEmpty()) {    
+            System.out.println("La agenda esta vacia");
+            return;
+        }
+
+        System.out.println("Lista de contactos:");
+        for (int i = 0; i < nombres.size(); i++) {
+            System.out.println((i + 1) + ". " + nombres.get(i) + " - " + telefonos.get(i));
+        }
     }
 }
